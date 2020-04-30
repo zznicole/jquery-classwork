31533966fc1f5c797ef5af4ec24d33684a21e3d1
Nicole Zheng <zznicole@hotmail.com>
2020-04-30 22:07:52 +0200

    apply jquery effects

---
+++ src/main.js
@@ -0,0 +1,24 @@
+
+$(".panel__title1,.panel__img1").on("click", ()=> {
+    $(".panel__1").fadeOut(1000).fadeIn(2000);
+});
+
+$(".panel__title2,.panel__img2").on("click", ()=> {
+    $(".panel__2").hide(3000).show(1000);
+});
+
+$(".panel__title3,.panel__img3").on("click", ()=> {
+    $(".panel__3").fadeOut(1000).fadeIn(2000);
+});
+
+$(".panel__4--content").on("click", ()=> {
+    $(".panel__4").fadeOut(1000).fadeIn(2000);
+});
+
+$(".panel__title5,.panel__img5").on("click", ()=> {
+    $(".panel__5").fadeOut(1000).fadeIn(2000);
+});
+
+$(".panel__title6,.panel__img6").on("click", ()=> {
+    $(".panel__6").fadeOut(1000).fadeIn(2000);
+});
